09e562cd9568dd4c841e0761a7ec07118bc8987d
ramshasank <pathath.ram@gmail.com>
2015-06-27 16:26:46 -0500

    tutorial4

---
+++ iOS-OpenCV-master-2-2/OpenCVSample/Classes/OpenCVViewController/ColorCircleViewController.h
@@ -0,0 +1,17 @@
+#import "AbstractOCVViewController.h"
+#import <RoboMe/RoboMe.h>
+#import <opencv2/imgproc/imgproc_c.h>
+#import <AVFoundation/AVFoundation.h>
+
+
+
+@interface ColorCircleViewController : AbstractOCVViewController<AVCaptureVideoDataOutputSampleBufferDelegate>
+{
+    double _min, _max;
+    AVCaptureSession *_session2;
+}
+
+
+
+
+@end
